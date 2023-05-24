bbc185fcd783a99d0df5585b889dc0a85ae70b11
viniciusgoneli <viniciusgoneli_2012@hotmail.com>
2023-05-24 19:50:14 -0300

    feat(added test libraries): installed and configured jest and testing library.

---
+++ __tests__/home.test.tsx
@@ -0,0 +1,13 @@
+import { render, screen } from "@testing-library/react";
+import React from "react";
+import Home from "../src/app/page";
+
+describe("Home", () => {
+	it("should render correctly", () => {
+		render(<Home />);
+
+		const text = screen.getByText("Ola mundo");
+
+		expect(text).toBeInTheDocument();
+	});
+});
