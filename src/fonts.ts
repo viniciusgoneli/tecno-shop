500b7882ded13fa2fe37809f8a9af109ff53390b
viniciusgoneli <viniciusgoneli_2012@hotmail.com>
2023-05-24 22:29:37 -0300

    feat(fonts): Added main fonts to project.

---
+++ src/fonts.ts
@@ -0,0 +1,16 @@
+import { Mulish, Ovo, Palanquin_Dark } from "next/font/google";
+
+export const palanquin_dark = Palanquin_Dark({
+	weight: "400",
+	subsets: ["latin"],
+});
+
+export const mulish = Mulish({
+	weight: "400",
+	subsets: ["latin"],
+});
+
+export const ovo = Ovo({
+	weight: "400",
+	subsets: ["latin"],
+});
