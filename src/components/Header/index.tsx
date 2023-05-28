c405ba203d4205a557c1052d814a6c8144f68849
viniciusgoneli <viniciusgoneli_2012@hotmail.com>
2023-05-28 18:02:21 -0300

    feat: Header component implemented

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,22 +1,46 @@
 import React from "react";
+import "../../styles/globals.css";
 import styles from "./Header.module.css";
+import Image from "next/image";
+import MenuIcon from "../../../public/icons/menu.svg";
+import SearchIcon from "../../../public/icons/search.svg";
+import BagIcon from "../../../public/icons/bag.svg";
+
+const ICON_SIZE = 24;
 
 export default function Header() {
 	return (
-		<header className={styles.header}>
+		<header className={styles.wrapper}>
 			<section className={styles.banner}>
-				<p className={styles.bannerText}>
-					Free Shipping On All U.S. Orders
-				</p>
+				<p>Free Shipping On All U.S. Orders</p>
 			</section>
-			<section>
-				<div>
-					<button></button>
-					<h1 className={styles.appName}>Participle+</h1>
-					<div>
-						<button></button>
-						<button></button>
-					</div>
+			<section className={styles.main}>
+				<button>
+					<Image
+						src={MenuIcon}
+						width={ICON_SIZE}
+						height={ICON_SIZE}
+						alt="Menu icon"
+					/>
+				</button>
+				<h1>Participle+</h1>
+				<div className={styles.row}>
+					<button>
+						<Image
+							src={SearchIcon}
+							width={ICON_SIZE}
+							height={ICON_SIZE}
+							alt="Search icon"
+						/>
+					</button>
+					<button>
+						<Image
+							src={BagIcon}
+							width={ICON_SIZE}
+							height={ICON_SIZE}
+							alt="Bag icon"
+						/>
+					</button>
 				</div>
 			</section>
 		</header>
