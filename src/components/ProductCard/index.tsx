0511d3234123cff4da3287b3d18ee2cbb0334450
viniciusgoneli <viniciusgoneli_2012@hotmail.com>
2023-06-07 20:59:16 -0300

    feat: wrapped ProductCard content in a Link with a mocked href.

--- src/components/ProductCard/index.tsx
+++ src/components/ProductCard/index.tsx
@@ -3,6 +3,7 @@ import React, { CSSProperties } from "react";
 import styles from "./ProductCard.module.css";
 import ImagesCarousel, { CarouselProps } from "../Carousel";
 import { ImageProps } from "@/types/imageProps";
+import Link from "next/link";
 
 export interface ProductCardProps {
 	title: string;
@@ -21,35 +22,43 @@ export default function ProductCard({
 }: ProductCardProps) {
 	return (
 		<div className={styles.wrapper} style={{ ...style, width: 140 }}>
-			<div className={styles.imgWrapper}>
-				<Image
-					data-testid="prod-card-img"
-					{...imgProps}
-					width={140}
-					height={179}
-				/>
-			</div>
-			<div className={styles.info}>
-				<h5>{title}</h5>
-				<div className={styles.priceWrapper}>
-					{discountPercent ? (
-						<p className={styles.priceWithDiscount}>
-							$
-							{(discountPercent * price).toFixed(2)}
-						</p>
-					) : (
-						<p className={styles.price}>
-							${price.toFixed(2)}
-						</p>
-					)}
-
-					{discountPercent ? (
-						<p className={styles.oldPrice}>
-							${price.toFixed(2)}
-						</p>
-					) : null}
+			<Link href={"/product/1"}>
+				<div className={styles.imgWrapper}>
+					<Image
+						data-testid="prod-card-img"
+						{...imgProps}
+						width={140}
+						height={179}
+					/>
 				</div>
-			</div>
+				<div className={styles.info}>
+					<h5>{title}</h5>
+					<div className={styles.priceWrapper}>
+						{discountPercent ? (
+							<p
+								className={
+									styles.priceWithDiscount
+								}
+							>
+								$
+								{(
+									discountPercent * price
+								).toFixed(2)}
+							</p>
+						) : (
+							<p className={styles.price}>
+								${price.toFixed(2)}
+							</p>
+						)}
+
+						{discountPercent ? (
+							<p className={styles.oldPrice}>
+								${price.toFixed(2)}
+							</p>
+						) : null}
+					</div>
+				</div>
+			</Link>
 		</div>
 	);
 }
