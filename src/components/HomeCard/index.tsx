2af28e062b0e5ab043d8e19032da36172b689753
viniciusgoneli <viniciusgoneli_2012@hotmail.com>
2023-06-13 20:10:04 -0300

    refactor: HomeCard

--- src/components/HomeCard/index.tsx
+++ src/components/HomeCard/index.tsx
@@ -1,28 +1,43 @@
-import Image, { ImageProps } from "next/image";
+import Image from "next/image";
 import React, { CSSProperties } from "react";
 import styles from "./HomeCard.module.css";
 import Carousel, { CarouselProps } from "../Carousel";
+import { ImageProps } from "@/types/imageProps";
+import Link from "next/link";
 
 interface HomeCardProps {
-	carouselProps: Omit<CarouselProps, "width">;
-	width: number;
+	images: ImageProps[];
 	title: string;
 	subtitle: string;
 	style?: CSSProperties;
+	carouselProps?: Omit<CarouselProps, "images">;
 }
 
 export default function HomeCard({
 	carouselProps,
-	width,
+	images,
 	title,
 	subtitle,
 	style,
 }: HomeCardProps) {
 	return (
-		<div className={styles.wrapper} style={{ ...style, width }}>
-			<Carousel {...carouselProps} width={width} autoplay />
+		<Link
+			className={styles.wrapper}
+			style={style}
+			href={"/collections/clothing"}
+		>
+			{images?.length > 1 ? (
+				<Carousel {...carouselProps} images={images} />
+			) : (
+				<Image
+					src={images[0].src}
+					alt={images[0].alt}
+					width={291}
+					height={420}
+				/>
+			)}
 			<h4>{title}</h4>
 			<p>{subtitle}</p>
-		</div>
+		</Link>
 	);
 }
