1196fcaa5faca1079639ea7cd7f5b7c5e81caab6
viniciusgoneli <viniciusgoneli_2012@hotmail.com>
2023-06-20 22:11:44 -0300

    refactor: HomePage refactored.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,69 +1,39 @@
 import styles from "./page.module.css";
-import React, { Suspense } from "react";
+import React from "react";
 import HomeCard from "@/components/HomeCard";
-import { ImageRefProps } from "@/types/imageProps";
-import FeaturedSection from "@/components/FeaturedSection";
 import { ref } from "firebase/storage";
-import { convertImgRefToImgSrc, storage } from "@/services/firebase";
+import {
+	getFirebaseDatabase,
+	getFirebaseStorage,
+} from "@/services/firebase/firebase";
+import ItemsSlider from "@/components/ItemsSlider";
+import { ImageRefProps } from "@/models/imageProps";
+import { orderByKey, query } from "firebase/database";
+import getFirebaseDatabaseRef from "@/services/firebase/getFirebaseDatabaseRef";
+import FeaturedSection from "@/components/FeaturedSection";
 
-const getFirstCardImages = async () => {
-	return await convertImgRefToImgSrc({
-		ref: ref(storage, "headsets/test1/1.jpg"),
-		alt: "Woman wearing a white dress",
-	});
-};
+const storage = getFirebaseStorage();
+const database = getFirebaseDatabase();
 
-const getSecondCardImage = async () => {
-	return await convertImgRefToImgSrc({
-		ref: ref(storage, "headsets/test1/4.jpg"),
-		alt: "Woman wearing a red dress",
-	});
-};
+/*const image1 = {
+	storageRef: ref(storage, "headsets/test1/1.jpg"),
+	alt: "Woman wearing a white dress",
+} satisfies ImageRefProps;
 
-const getThirdCardImage = async () => {
-	return await convertImgRefToImgSrc({
-		ref: ref(storage, "headsets/test1/5.jpg"),
-		alt: "Woman wearing a blue dress",
-	});
-};
+const image2 = {
+	storageRef: ref(storage, "headsets/test1/2.jpg"),
+	alt: "Woman wearing a red dress",
+} satisfies ImageRefProps;
 
-export default async function Home() {
-	const firstCardImgsPromise = getFirstCardImages();
-	const secondCardImgPromise = getSecondCardImage();
-	const thirdCardImgPromise = getThirdCardImage();
+const image3 = {
+	storageRef: ref(storage, "headsets/test1/3.jpg"),
+	alt: "Woman wearing a blue dress",
+} satisfies ImageRefProps;*/
 
+export default function Home() {
 	return (
 		<main className={styles.main}>
-			<h2 className={styles.title}>DESTAQUES</h2>
-			<section className={styles.topSection}>
-				<HomeCard
-					carouselProps={{
-						autoplay: true,
-					}}
-					title="parks and recreation"
-					subtitle="shop new"
-					imagePromise={firstCardImgsPromise}
-					style={{ marginBottom: 18 }}
-				/>
-				<HomeCard
-					carouselProps={{
-						autoplay: true,
-					}}
-					title="learning in"
-					subtitle="shop dresses"
-					imagePromise={secondCardImgPromise}
-					style={{ marginBottom: 18 }}
-				/>
-				<HomeCard
-					carouselProps={{
-						autoplay: true,
-					}}
-					title="all over velvet"
-					subtitle="shop back-in-stock"
-					imagePromise={thirdCardImgPromise}
-					style={{ marginBottom: 18 }}
-				/>
-			</section>
+			<FeaturedSection />
 		</main>
 	);
 }
