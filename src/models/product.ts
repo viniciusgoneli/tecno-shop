7e17a1dfaa5130673a4e6df24db2437bb5e6b134
viniciusgoneli <viniciusgoneli_2012@hotmail.com>
2023-07-12 22:24:23 -0300

    refactor: updated product models.

--- src/models/product.ts
+++ src/models/product.ts
@@ -1,6 +1,8 @@
 export interface Product {
+	id: string;
 	name: string;
 	description: string;
+	techInfo: string;
 	images: ProductImage[];
 	categories: ProductCategory[];
 	price: number;
@@ -16,14 +18,20 @@ export interface Product {
 export interface ProductImage {
 	productId: string;
 	src: string;
+	alt: string;
 	position: number;
 	createdAt: string;
 	updatedAt: string;
 }
 
 export interface ProductCategory {
+	id: string;
 	name: string;
+	handleName: string;
 	description: string;
 	createdAt: string;
 	updatedAt: string;
+	imageUrl: string;
+	subcategories: string[];
+	parent: string;
 }
