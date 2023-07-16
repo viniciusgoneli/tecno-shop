beba4f176a135e9660f4b14cba644e6d910155a8
viniciusgoneli <viniciusgoneli_2012@hotmail.com>
2023-07-16 20:05:19 -0300

    feat: added 'formatCartItemsCount' function to 'functions' module.

--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -1,5 +1,11 @@
 export const convertJsonToArray = (json: object) => {
+	if (!json) return [];
+
 	return Object.entries(json).map(([key, value]) => {
 		return { id: key, ...value };
 	});
 };
+
+export function formatCartItemsCount(count: number) {
+	return count > 99 ? "+99" : count.toString();
+}
