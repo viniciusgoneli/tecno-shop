b6cc6ad48050663d34083acfbf40a607bc66fecf
viniciusgoneli <viniciusgoneli_2012@hotmail.com>
2023-07-22 22:19:25 -0300

    feat: added 'CategoriesSection' on HomePage.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,39 +1,13 @@
 import styles from "./page.module.css";
 import React from "react";
-import HomeCard from "@/components/HomeCard";
-import { ref } from "firebase/storage";
-import {
-	getFirebaseDatabase,
-	getFirebaseStorage,
-} from "@/services/firebase/firebase";
-import ItemsSlider from "@/components/ItemsSlider";
-import { ImageRefProps } from "@/models/imageProps";
-import { orderByKey, query } from "firebase/database";
-import getFirebaseDatabaseRef from "@/services/firebase/getFirebaseDatabaseRef";
 import FeaturedSection from "@/components/FeaturedSection";
-
-const storage = getFirebaseStorage();
-const database = getFirebaseDatabase();
-
-/*const image1 = {
-	storageRef: ref(storage, "headsets/test1/1.jpg"),
-	alt: "Woman wearing a white dress",
-} satisfies ImageRefProps;
-
-const image2 = {
-	storageRef: ref(storage, "headsets/test1/2.jpg"),
-	alt: "Woman wearing a red dress",
-} satisfies ImageRefProps;
-
-const image3 = {
-	storageRef: ref(storage, "headsets/test1/3.jpg"),
-	alt: "Woman wearing a blue dress",
-} satisfies ImageRefProps;*/
+import CategoriesSection from "@/components/CategoriesSection";
 
 export default function Home() {
 	return (
 		<main className={styles.main}>
-			<FeaturedSection />
+			<FeaturedSection style={{ marginBottom: 20 }} />
+			<CategoriesSection />
 		</main>
 	);
 }
