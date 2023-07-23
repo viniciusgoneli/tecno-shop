2942e43104763f89c3816f5a7a1300165115848b
viniciusgoneli <viniciusgoneli_2012@hotmail.com>
2023-07-22 22:20:06 -0300

    feat: created 'formatPrice' function.

--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -1,11 +1,13 @@
-export const convertJsonToArray = (json: object) => {
-	if (!json) return [];
-
-	return Object.entries(json).map(([key, value]) => {
-		return { id: key, ...value };
-	});
-};
-
 export function formatCartItemsCount(count: number) {
 	return count > 99 ? "+99" : count.toString();
+}
+
+export function formatPrice(price: number) {
+	const formattedPrice = price.toLocaleString("pt-BR", {
+		style: "currency",
+		currency: "BRL",
+		maximumFractionDigits: 2,
+	});
+
+	return formattedPrice;
 }
