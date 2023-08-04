46b48651e3eeaaed02f4c0261d50249be641c63e
viniciusgoneli <viniciusgoneli_2012@hotmail.com>
2023-08-04 15:11:31 -0300

    feat: added categories ordering and filtering on 'Categories' page.

--- src/app/categories/[...categories]/page.tsx
+++ src/app/categories/[...categories]/page.tsx
@@ -1,30 +1,35 @@
-import React from "react";
+"use client";
+
+import React, { useEffect, useState } from "react";
 import styles from "./page.module.css";
-import CollectionsHeader from "@/components/CollectionsHeader";
+import CollectionsHeader, { SortType } from "@/components/CategoriesHeader";
+import ProductsSection from "@/components/ProductsSection";
 import ProductCard from "@/components/ProductCard";
 import { getFirestoreDatabase } from "@/services/firebase/firebase";
-import { Params } from "next/dist/shared/lib/router/utils/route-matcher";
 import {
+	OrderByDirection,
 	collection,
 	getDocs,
-	limit,
 	orderBy,
 	query,
 	where,
 } from "firebase/firestore";
 import { Product } from "@/models/product";
+import { Params } from "next/dist/shared/lib/router/utils/route-matcher";
+import { useParams } from "next/navigation";
+import Loading from "@/components/Loading";
+import SadIcon from "../../../../public/svg/sad-icon.svg";
+import Image from "next/image";
 
 const db = getFirestoreDatabase();
 
-interface CollectionsPageProps {
-	params: Params;
-}
-
-const getProducts = async (route: string) => {
+const getProducts = async (route: string, sortType: SortType) => {
 	const prodsCollectionRef = collection(db, "products");
+	const [field, order] = sortType.split("-");
 	const q = query(
 		prodsCollectionRef,
-		where("categories", "array-contains-any", [route])
+		where("categories", "array-contains-any", [route]),
+		orderBy(field, order as OrderByDirection)
 	);
 	const prodsDocs = (await getDocs(q))?.docs;
 
@@ -33,25 +38,67 @@ const getProducts = async (route: string) => {
 		...doc.data(),
 	}));
 
-	return products;
+	return products as Product[];
 };
 
-export default async function CollectionsPage({
-	params,
-}: CollectionsPageProps) {
-	const products = await getProducts(params?.categories?.pop());
+export default function CategoriesPage() {
+	const categories = useParams().categories.split("/");
+
+	const [loading, setLoading] = useState(true);
+	const [products, setProducts] = useState<Product[]>([]);
+	const [sortType, setSortType] = useState<SortType>("createdAt-asc");
+
+	const loadProducts = async () => {
+		setLoading(true);
+
+		const products = await getProducts(
+			categories[categories.length - 1],
+			sortType
+		);
+
+		setProducts(products);
+		setLoading(false);
+	};
+
+	useEffect(() => {
+		loadProducts();
+	}, [sortType]);
 
 	return (
-		<main className={styles.wrapper}>
-			<CollectionsHeader />
+		<section className={styles.wrapper}>
+			<CollectionsHeader
+				sortType={sortType}
+				onChangeSortType={setSortType}
+			/>
 			<section className={styles.productsSection}>
-				{products?.map((it) => (
-					<ProductCard
-						item={it}
-						style={{ marginBottom: 20 }}
-					/>
-				))}
+				{loading ? (
+					<div className={styles.noItems}>
+						<Loading />
+					</div>
+				) : !products.length ? (
+					<div className={styles.noItems}>
+						<Image
+							src={SadIcon}
+							width={50}
+							height={50}
+							color="#000"
+							alt="Sad icon"
+						/>
+						<h2>
+							Desculpe, não encontramos nenhum
+							produto na categoria selecionada.
+						</h2>
+					</div>
+				) : (
+					products?.map((it) => (
+						<ProductCard
+							key={it.id}
+							item={it}
+							style={{ marginBottom: 20 }}
+						/>
+					))
+				)}
 			</section>
-		</main>
+		</section>
 	);
 }
