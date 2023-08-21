81fba86776f899fe71e3961cb23a4dd2930df278
viniciusgoneli <viniciusgoneli_2012@hotmail.com>
2023-08-21 17:34:23 -0300

    refactor: changed HomePage css modules to Styled Components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,13 @@ import styles from "./page.module.css";
 import React from "react";
 import FeaturedSection from "@/components/FeaturedSection";
 import CategoriesSection from "@/components/CategoriesSection";
+import { Container } from "./pageStyle";
 
 export default function Home() {
 	return (
-		<main className={styles.main}>
+		<Container>
 			<FeaturedSection style={{ marginBottom: 20 }} />
 			<CategoriesSection />
-		</main>
+		</Container>
 	);
 }
---
+++ src/app/pageStyle.ts
@@ -0,0 +1,9 @@
+"use client";
+
+import { headerHeight } from "@/utils/constants";
+import styled from "styled-components";
+
+export const Container = styled.main`
+	width: 100%;
+	margin-top: ${headerHeight}px;
+`;
