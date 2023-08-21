0009ff602ee31bc911fe4859b375e955573bb73e
viniciusgoneli <viniciusgoneli_2012@hotmail.com>
2023-08-21 17:32:41 -0300

    refactor: transfered some css constants to typescript file.

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,2 +1,7 @@
-export const cookiePermissionKey = "permission";
-export const cartStorageKey = "cart_items";
+// Colors
+export const grayColor = "#F7F7F7";
+export const midGrayColor = "#828282";
+export const darkGrayColor = "#414141";
+
+// Dimensions
+export const headerHeight = 70;
